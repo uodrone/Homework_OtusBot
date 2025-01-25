78198a474cf3b48e48a1644e6bfc2da1bb8c27f0
uodrone <drone_86@mail.ru>
2025-01-25 09:43:39 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using static Telegram.Bot.TelegramBotClient;
+
+namespace MyOtusBot
+{
+    internal class Program
+    {
+        private static string _botToken = "7716203098:AAHBqQWCPqLa5eGDeQ91ICZN8zA_s-NU1m0";
+        static async Task Main(string[] args)
+        {
+            var botClient = new TelegramBotClient(_botToken);
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = [UpdateType.Message],
+                DropPendingUpdates = true
+            };
+            var handler = new UpdateHandler();
+            
+            
+
+            try
+            {
+                botClient.StartReceiving(handler, receiverOptions);
+                handler.OnHandleUpdateStarted += Handler_OnHandleUpdateStarted;
+                handler.OnHandleUpdateCompleted += Handler_OnHandleUpdateCompleted;
+                var me = await botClient.GetMe();
+                Console.WriteLine($"{me.FirstName} запущен!");
+            }
+            finally
+            {
+                // Отписываемся от событий
+                handler.OnHandleUpdateStarted -= Handler_OnHandleUpdateStarted;
+                handler.OnHandleUpdateCompleted -= Handler_OnHandleUpdateCompleted;
+            }
+
+
+            await Task.Delay(-1);
+        }
+
+        private static void Handler_OnHandleUpdateCompleted(string message)
+        {
+            Console.WriteLine($"Закончилась обработка сообщения {message}");
+        }
+
+        private static void Handler_OnHandleUpdateStarted(string message)
+        {
+            Console.WriteLine($"Началась обработка сообщения {message}");
+        }
+    }
+}
---
+++ UpdateHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace MyOtusBot
+{
+    internal class UpdateHandler : IUpdateHandler
+    {
+        public delegate void MessageHandler(string message);
+        // События, которые будут вызываться в начале и конце обработки сообщения
+        public event MessageHandler OnHandleUpdateStarted;
+        public event MessageHandler OnHandleUpdateCompleted;
+        public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.Message)
+            {
+                // Вызываем событие начала обработки
+                OnHandleUpdateStarted?.Invoke(update.Message.Text);
+                Console.WriteLine("Сообщение успешно принято");
+                await botClient.SendTextMessageAsync(
+                    chatId: update.Message.Chat.Id,
+                    text: update.Message.Text,
+                    cancellationToken: cancellationToken);
+                // Вызываем событие завершения обработки
+                OnHandleUpdateCompleted?.Invoke(update.Message.Text);
+            }            
+        }
+    }
+}
